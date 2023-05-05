f34f036378559d1a26da8a39989ccbf73110dfbf
PedroHenriqueLOURENCO <rm98402@fiap.com.br>
2023-05-05 20:14:29 -0300

    concatenação de arrays com spread

--- JS/script.js
+++ JS/script.js
@@ -23,8 +23,44 @@
 
 // -----------------------------------------------------------------------------------;
 
-let indice = frutas.indexOf("amora");
-console.log(`elemento do índice antes da remoção : ${frutas[indice]}`);
-frutas.splice(indice,1);
-console.log(frutas);
-console.log(`elemento no índice após a remoção : ${frutas[indice]}`);
+// let indice = frutas.indexOf("amora");
+// console.log(`elemento do índice antes da remoção : ${frutas[indice]}`);
+// frutas.splice(indice,1);
+// console.log(frutas);
+// console.log(`elemento no índice após a remoção : ${frutas[indice]}`);
+
+// console.table(frutas)
+// frutas.forEach( (frutas)=>{
+//     console.log("fruta da vez : " + frutas);
+// } );
+
+//----------------------------------------------------------------------------------------;
+
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// console.log("array 1 : " + nr1);
+// console.log(" array 2 : "+ nr2);
+
+// let nr3 = [nr1,nr2];
+// console.log("novo array : " + nr3);
+
+// nr3.array.forEach((nr) => {
+//     nr.forEach((n)=>{
+//         console.log("itens do novo array: " + n)
+//     });
+//     console.log("itens do novo array : " + nr);
+// });
+
+//----------------------------------------------------------------------------------------;
+
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+console.log("array 1 : " + nr1);
+console.log(" array 2 : "+ nr2);
+
+let nr3 = [...nr1,...nr2];
+console.log("novo array : " + nr3);
+
+nr3.forEach((nr) => {
+        console.log("itens do novo array: " + nr)
+    });
